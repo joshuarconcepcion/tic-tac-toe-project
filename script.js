677ca699fc97e606c159d9cc3a623a28591b5cdd
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-06-12 13:58:36 -0700

    Added some js logic. GameBoard object, Player factory function, and working on GameController object

---
+++ script.js
@@ -0,0 +1,57 @@
+const GameBoard = (function () { //IIFE (immediately invoked function expression) assigned to GameBoard object
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+    function getBoard() {
+        return [...board]; 
+        //... used to return copy of board; 
+        //outside code cannot mutate original board
+    }
+
+    function resetBoard() {
+        board.fill('');
+    }
+
+    function markCell(index, mark) {
+        if (board[index] == '') {
+            board[index] = mark;
+        }
+        //if cell (board[index]) is empty, mark it
+    }
+
+    return {getBoard, resetBoard, markCell};
+})(); // () used to immediately run function and returns object with these methods
+
+function Player(name, mark) {
+    return {name, mark}; 
+    //return an object
+    //used like: const Joshua = Player('Joshua', 'X');
+    //so now Joshua is an object
+}
+
+const GameController = (function () {
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+    let currentPlayer = player1;
+
+    function switchPlayer() {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        //is currentPlayer player1? if so, then switch to player2
+        //if not, switch to player1
+    }
+
+    function checkWin() {
+        const board = GameBoard.getBoard();
+        const wins = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+            [0, 4, 8], [2, 4, 6] //diagonals
+        ];
+
+        return wins.some(([a, b, c]) => b[a] && board[a] === board[b] && board[b] === board[c] && board[a] === board[c]);
+        //iterates over each array in wins
+        //[a,b,c] destructures each win array
+        //return true if any of the win arrays match what's on the board
+        //.some iterates over array and returns true based on function inside
+    }
+
+})();
